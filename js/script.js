61d98420f4fc85ad07103ae455d52342867f3a03
DevFlavien <99437459+DevFlavien@users.noreply.github.com>
2023-07-26 23:00:39 +0200

    Ajout pause vidéo youtube au slide

--- js/script.js
+++ js/script.js
@@ -8,10 +8,58 @@ const nextBtn = slider.querySelector(".next-btn");
 let currentVideoIndex = 0;
 let numVideos = videos.length;
 
+    // Chargement de l'API YouTube Player
+    var tag = document.createElement('script');
+    tag.src = "https://www.youtube.com/iframe_api";
+    var firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+    // Variable pour stocker le lecteur YouTube
+    var player1;
+    var player2;
+    var player3;
+    var player4;
+    var player5;
+
+    // Fonction appelée lorsque l'API est prête
+    function onYouTubeIframeAPIReady() {
+      // Création du lecteur YouTube
+      player1 = new YT.Player('player1', {
+        height: '360',
+        width: '640',
+        videoId: 'rj71jMjNbLo', 
+      });
+
+      player2 = new YT.Player('player2', {
+        height: '360',
+        width: '640',
+        videoId: 'a3yIqtoEkpA', 
+      });
+
+      player3 = new YT.Player('player3', {
+        height: '360',
+        width: '640',
+        videoId: 'VYA6WGHUpdg',
+      });
+
+      player4 = new YT.Player('player4', {
+        height: '360',
+        width: '640',
+        videoId: 'eBEaPTNYCQY', 
+      });
+
+      player5 = new YT.Player('player5', {
+        height: '360',
+        width: '640',
+        videoId: 'lCofrhjIumY',
+      });
+    }
+
 // Cacher toutes les vidéos sauf la première
 for (let i = 1; i < numVideos; i++) {
   videos[i].style.display = "none";
 }
+
 
 // Gestionnaires d'événements pour les boutons de navigation
 prevBtn.addEventListener("click", () => {
@@ -19,6 +67,7 @@ prevBtn.addEventListener("click", () => {
   if (currentVideoIndex < 0) {
     currentVideoIndex = numVideos - 1;
   }
+
   updateSlider();
 });
 
@@ -27,18 +76,31 @@ nextBtn.addEventListener("click", () => {
   if (currentVideoIndex >= numVideos) {
     currentVideoIndex = 0;
   }
+
   updateSlider();
 });
+
+//fonction pause de toute les vidéos du carrousel dans le cas du clique sur prev ou next
+function pauseVideo() {
+  player1.pauseVideo();
+  player2.pauseVideo();
+  player3.pauseVideo();
+  player4.pauseVideo();
+  player5.pauseVideo();
+}
 
 // Fonction pour mettre à jour le slider
 function updateSlider() {
   for (let i = 0; i < numVideos; i++) {
     if (i === currentVideoIndex) {	videos[i].style.display = "block";
+ 
   } else {
     videos[i].style.display = "none";
   }
+  pauseVideo();
 }
 }
+
 
 //slider img
 
